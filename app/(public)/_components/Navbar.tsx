70e47a10c930f8467f3888fd446998f623094a8e
Joe Clark <weirdlookingjay@live.com>
2025-07-03 13:37:48 -0400

    04: landing page

---
+++ app/(public)/_components/Navbar.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { ThemeToggle } from "@/components/themeToggle";
+import { buttonVariants } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import Image from "next/image";
+import Link from "next/link";
+import UserDropdown from "./UserDropdown";
+
+const navigationItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Courses",
+    href: "/courses",
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+];
+
+export function Navbar() {
+  const { data: session, isPending } = authClient.useSession();
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[background-filter]:bg-background/60">
+      <div className="comtainer flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+        <Link href="/" className="flex items-center space-x-2 mr-4">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={50}
+            height={50}
+            className="size-9"
+          />
+          <span className="font-bold">LMS</span>
+        </Link>
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center space-x-2">
+            {navigationItems.map((item) => (
+              <Link
+                href={item.href}
+                key={item.name}
+                className="text-sm font-medium transistion-colors hover:text-primary"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="flex items-center justify-center space-x-4">
+            <ThemeToggle />
+            {isPending ? null : session ? (
+              <UserDropdown
+                name={session.user.name}
+                email={session.user.email}
+                image={session.user.image || ""}
+              />
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link href="/login" className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
